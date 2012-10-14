971a08282d9c54ae34b0f1f138fb12cda74b4aff
Joshua Coles <joshuac@static-52.home>
2012-10-14 07:11:42 +0100

    First Import of code.

---
+++ Code/commands.rb
@@ -0,0 +1,53 @@
+require 'open3'
+
+module Commands
+  puts "Starting"
+
+  LAUNCH_COMMAND = '/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/bin/java -Xms512M -Xmx1024M -Xincgc -cp "/Applications/Minecraft.app/Contents/Resources/Java/MinecraftLauncher.jar" net.minecraft.LauncherFrame'
+
+  def Commands.run_shell_command(command)
+    output = ""
+    errors = ""
+    puts "Running Command [#{command}]"
+    Open3.popen3(command) do |stdin, stdout, stderr|
+      output = stdout.read
+      errors = stderr.read
+    end
+    puts "  Output [#{output}]"
+    puts "  Errors [#{errors}]"
+  end
+
+  def Commands.prep
+    Dir.chdir("vinilla")
+      run_shell_command("cp minecraft.jar ../Code/mcp/jars")
+    Dir.chdir("../")
+  end
+
+  def Commands.patch
+    Dir.chdir("../patches")
+    run_shell_command("patch */*")
+    Dir.chdir("../Code")
+  end
+
+  def Commands.install
+    Dir.chdir("mcp")
+    Dir.chdir("forge")
+    run_shell_command("./install.sh")
+    Dir.chdir("../..")
+  end
+
+  def Commands.remake
+    Dir.chdir("mcp")
+    run_shell_command("./recompile.sh")
+    run_shell_command("./reobfuscate.sh")
+    Dir.chdir("../")
+  end
+
+  def Commands.run
+    run_shell_command(LAUNCH_COMMAND)
+  end
+
+  def Commands.foo
+    puts " Test"
+  end
+end
---
+++ Code/run.rb
@@ -0,0 +1,8 @@
+puts "Starting..."
+
+require_relative "commands"
+
+Commands.foo
+Commands.run
+
+puts "Finished..."
